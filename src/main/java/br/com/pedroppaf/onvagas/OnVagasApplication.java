b4147a0b020c2675fc248e6a786d25fd475cc03f
Pedro <pedroppaf@gmail.com>
2026-06-08 15:53:10 -0300

    initial commit

---
+++ src/main/java/br/com/pedroppaf/onvagas/OnVagasApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedroppaf.onvagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnVagasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnVagasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedroppaf/onvagas/OnVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedroppaf.onvagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
