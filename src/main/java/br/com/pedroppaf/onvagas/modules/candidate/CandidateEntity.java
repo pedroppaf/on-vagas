8c50af0f65bd2a82ecacda661cd9c706ed0f9241
Pedro <pedroppaf@gmail.com>
2026-06-09 14:37:29 -0300

    add global exception handeler

--- src/main/java/br/com/pedroppaf/onvagas/modules/candidate/CandidateEntity.java
+++ src/main/java/br/com/pedroppaf/onvagas/modules/candidate/CandidateEntity.java
@@ -1,6 +1,7 @@
 package br.com.pedroppaf.onvagas.modules.candidate;
 
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 import org.hibernate.validator.constraints.Length;
@@ -13,13 +14,14 @@ public class CandidateEntity {
 
     private String name;
 
-    @Pattern(regexp = "^(?!\\s*$).+", message = "O campo [username] não deve conter espaço")
+    @NotBlank
+    @Pattern(regexp = "\\S+", message = "O campo [username] não deve conter espaço")
     private String username;
 
     @Email(message = "O campo [email] deve conter um e-mail válido")
     private String email;
 
-    @Length(min = 10, max = 100)
+    @Length(min = 10, max = 100, message = "A senha deve conter entre 10 e 100 caracteres")
     private String password;
 
     private String description;
