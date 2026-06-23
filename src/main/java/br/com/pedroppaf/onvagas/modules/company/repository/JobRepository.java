42538be88079f780874db64afd04a867b487b015
Pedro <pedroppaf@gmail.com>
2026-06-23 12:47:20 -0300

    add documentation with swagger and list all jobs filter

--- src/main/java/br/com/pedroppaf/onvagas/modules/company/repository/JobRepository.java
+++ src/main/java/br/com/pedroppaf/onvagas/modules/company/repository/JobRepository.java
@@ -2,7 +2,10 @@ package br.com.pedroppaf.onvagas.modules.company.repository;
 
 import br.com.pedroppaf.onvagas.modules.company.entity.JobEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 import java.util.UUID;
 
 public interface JobRepository extends JpaRepository<JobEntity, UUID> {
+
+    List<JobEntity> findByDescriptionContainingIgnoreCase(String filter);
 }
