e415b71ceddc5571d0c5d002c6b4db71a1649db2
Pedro <pedroppaf@gmail.com>
2026-06-30 12:22:08 -0300

    add h2 for test and finalize job controller test

--- src/main/java/br/com/pedroppaf/onvagas/modules/company/controller/JobController.java
+++ src/main/java/br/com/pedroppaf/onvagas/modules/company/controller/JobController.java
@@ -4,7 +4,6 @@ import br.com.pedroppaf.onvagas.modules.company.dto.CreateJobDTO;
 import br.com.pedroppaf.onvagas.modules.company.entity.JobEntity;
 import br.com.pedroppaf.onvagas.modules.company.service.CreateJobService;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -14,12 +13,12 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.util.UUID;
 
 @RestController
@@ -37,16 +36,22 @@ public class JobController {
             @ApiResponse(responseCode = "200", content = {@Content(schema = @Schema(implementation = JobEntity.class))})
     })
     @SecurityRequirement(name = "jwt_auth")
-    public JobEntity create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
+    public ResponseEntity<Object> create(@Valid @RequestBody CreateJobDTO createJobDTO, HttpServletRequest request){
         var companyId = request.getAttribute("company_id");
 
-        var jobEntity = JobEntity.builder()
-                .benefits(createJobDTO.getBenefits())
-                .companyId(UUID.fromString(companyId.toString()))
-                .description(createJobDTO.getDescription())
-                .level(createJobDTO.getLevel())
-                .build();
+        try {
+            var jobEntity = JobEntity.builder()
+                    .benefits(createJobDTO.getBenefits())
+                    .companyId(UUID.fromString(companyId.toString()))
+                    .description(createJobDTO.getDescription())
+                    .level(createJobDTO.getLevel())
+                    .build();
 
-        return this.jobService.execute(jobEntity);
+            var result = this.jobService.execute(jobEntity);
+            return ResponseEntity.ok().body(result);
+
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 }
